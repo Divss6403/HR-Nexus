807e176cc150bf5454df08793949f999f7281517
Divss64 <Divss64@users.noreply.github.com>
2026-01-10 19:45:17 +0530

    changes

--- backend/server.py
+++ backend/server.py
@@ -70,7 +70,7 @@ async def shutdown_event():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://hr-nexus-app.onrender.com"],
+    allow_origins=["https://hr-nexus-app.onrender.com","http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
